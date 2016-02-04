2577c97b9842b24f05081499025b544e0d685693
Jacob <JacobTheEldest@gmail.com>
2016-02-04 09:06:02 -0500

    adjustments. still doesn't work reliably.

--- aging_report.py
+++ aging_report.py
@@ -5,8 +5,8 @@ Scrapes aging report info from mrmprod.
 '''
 
 import os
-import getpass
 import datetime
+from selenium_jacob import *
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
@@ -14,13 +14,13 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 def main():
     #Retrieve login info
-    (username, password) = login()
+    (username, password) = login_info('makor_login')
 
     #Open Firefox
     #Give it time to load
     global browser
     browser = webdriver.Firefox()
-    browser_load_wait('http://mrmprod/Login.aspx')
+    url_wait(browser, 'http://mrmprod/Login.aspx')
 
     #LOGIN
     #Login page find elements
@@ -28,7 +28,7 @@ def main():
     password_box_id = 'ctl00_ContentPlaceHolder1_Login_Password'
     login_submit_id = 'ctl00_ContentPlaceHolder1_Login_LoginButton'
     #Give it time to load
-    wait_for_element(username_box_id, 'id')
+    wait_for_element(browser, username_box_id, 'id')
     browser.find_element_by_id(username_box_id).send_keys(username)
     browser.find_element_by_id(password_box_id).send_keys(password)
     browser.find_element_by_id(login_submit_id).click()
@@ -37,7 +37,7 @@ def main():
     summary_url = 'http://mrmprod/ProcessSteps/AssetRecoverySummary.aspx?LotNumber=-99&PageSize=999'
     skid_table_id = 'ctl00_CPH1_ResaleSummaryGrid_GridView1'
     browser.get(summary_url)
-    wait_for_element(skid_table_id, 'id')
+    wait_for_element(browser, skid_table_id, 'id')
 
     #Open output file
     now = datetime.datetime.now()
@@ -48,33 +48,33 @@ def main():
     num_skids = len(browser.find_elements_by_class_name('rbSecondary'))
     for button_num in range(0, num_skids):
         browser.get(summary_url)
-        wait_for_element(skid_table_id, 'id')
+        wait_for_element(browser, skid_table_id, 'id')
         process_button_els = browser.find_elements_by_class_name('rbSecondary')
         current_button_id = process_button_els[button_num].get_attribute('id')
         age = browser.find_element_by_id('{}lblDaysRecieved'.format(current_button_id[:-26])).text
         process_button_els[button_num].click()
         unit_table_id = 'ctl00_CPH1_ccTransGrid1_gvReceivingTransactions'
-        wait_for_element(unit_table_id, 'id')
+        wait_for_element(browser, unit_table_id, 'id')
         skid_url = browser.current_url
 
         num_units = len(browser.find_elements_by_xpath('/html/body/form/div[8]/div[13]/div[1]/div[1]/div/table/tbody//*[@value="Test"]'))
         num_pages = 1
         while True:
-            if element_exists(str(num_pages + 1), 'link'):
+            if element_exists(browser, str(num_pages + 1), 'link'):
                 num_pages += 1
             else:
                 break
         for page in range(1, num_pages + 1):
             browser.get(skid_url)
-            wait_for_element(unit_table_id, 'id')
+            wait_for_element(browser, unit_table_id, 'id')
             if page != 1:
                 browser.find_element_by_link_text(page)
-                wait_for_element(unit_table_id, 'id')
+                wait_for_element(browser, unit_table_id, 'id')
             for unit in range(0, num_units):
                 test_button_els = browser.find_elements_by_xpath('/html/body/form/div[8]/div[13]/div[1]/div[1]/div/table/tbody//*[@value="Test"]')
                 test_button_els[unit].click()
                 asset_info_table_id = 'ctl00_CPH1_ccTransGrid1_rmaAssetInformation_fsAssetInfoControls'
-                wait_for_element(asset_info_table_id, 'id')
+                wait_for_element(browser, asset_info_table_id, 'id')
 
                 lot_id = 'ctl00_CPH1_ccTransGrid1_DetailsHeader_lblLotNumber'
                 unit_type_id = 'ctl00_CPH1_ccTransGrid1_rmaAssetInformation_lblClassName'
@@ -98,53 +98,6 @@ def main():
     #Cleanup
     output_file.close()
 
-#Continuously attempt to open a url until it succeeds.
-def browser_load_wait(url):
-    while True:
-        try:
-            browser.get(url)
-        except:
-            continue
-        break
-
-#Look for element existence
-def element_exists(identifier, method='xpath'):
-    '''
-    Returns True if it can find the specified element
-    '''
-    try:
-        if method == 'xpath':
-            element = WebDriverWait(browser, 1).until(
-                EC.presence_of_element_located((By.XPATH, identifier))
-            )
-        elif method == 'id':
-            element = WebDriverWait(browser, 1).until(
-                EC.presence_of_element_located((By.ID, identifier))
-            )
-        elif method == 'link':
-            element = WebDriverWait(browser, 1).until(
-                EC.presence_of_element_located((By.LINK_TEXT, identifier))
-            )
-    except:
-        return False
-    return True
-
-#Wait until element can be found
-def wait_for_element(identifier, method='xpath'):
-    wait = True
-    count = 0
-    while wait and count < 100:
-        wait = not element_exists(identifier, method)
-        count += 1
-    return
-
-#Ask for login info
-def login():
-    #username = input('Username: ')
-    #password = getpass.getpass('Password: ')
-    username = 'jacobs'
-    password = 'Drummer1'
-    return (username, password)
-
+#Call main() function
 if __name__ == '__main__':
     main()
